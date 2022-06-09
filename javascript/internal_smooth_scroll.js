0a1711304e59d343e89f60b9a245e3ca0fdd5401
monokuro_megane <monokuro_megane_contact@yahoo.co.jp>
2022-06-09 15:14:45 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Changes to be committed:
          modified:   index.html
          new file:   javascript/internal_smooth_scroll.js

---
+++ javascript/internal_smooth_scroll.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('a[href^="#"]').on('click', function() {
+    var speed = 800;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({
+      scrollTop: position
+    }, speed, 'swing');
+    return false;
+  });
+});
